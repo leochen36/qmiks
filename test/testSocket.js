25fd998f987a75c95df26c6bd98e597b73cc5f8e
cwq0312@163.com <cwq0312@163.com>
2013-03-05 09:54:41 +0800

    提交

--- test/testSocket.js
+++ test/testSocket.js
@@ -6,18 +6,20 @@
 (function() {
 
     var Q = require("../src/Qmiks.Server");
-    require("../src/Qmiks.Security.MD5");
+    var crypto = require("crypto");
+    require("../src/Qmiks.Security");
 
     var net = require('net'),
         length = 128;
 
 
 
-    var mproto="GET / HTTP/1.1";
-    var mkey="Sec-WebSocket-Key: ";
+    var mproto = "GET / HTTP/1.1";
+    var mkey = "Sec-WebSocket-Key: ";
 
     var server = net.createServer(function(stream) {
         try {
+
             //stream.setEncoding('utf8');
             stream.on('connect', function(e) {
                 console.log("connect");
@@ -25,77 +27,86 @@
                 stream.write('hello');
 
             });
+            stream.on("secure",function(){
+                console.log(">>>>>>>>>>>>>>secure");
 
+            })
             function resume() {
                 stream.resume();
             }
             //  pause();
             stream.on('data', function(buffer) {
-                 stream.pause();
-                 console.log("------------------------------------------------------------------");
-                //console.log(buffer);
-                var list=[];
-                for(var i=0;i<buffer.length;i++){
-                    var charValue= String.fromCharCode(buffer[i]); 
+                stream.pause();
+                console.log("------------------------------------------------------------------");
+                console.log(buffer);
+                var list = [];
+                for (var i = 0; i < buffer.length; i++) {
+                    var charValue = String.fromCharCode(buffer[i]);
                     //console.log(charValue+"-"+buffer[i]);
                 }
 
-                var i,chr,tlength=0,tstart=0;
-                for(i=0;i<buffer.length;i++){
-                    chr=buffer[i];
-                    if(chr==10&&tstart!=i-1){
-                        console.log("start:"+tstart+",length:"+(i-1));
-                        if(buffer[i-1]==13){
-                            nb=buffer.slice(tstart,i-1);   
-                        }else{
-                            nb=buffer.slice(tstart,i);
+                var i, chr, tlength = 0,
+                    tstart = 0;
+                for (i = 0; i < buffer.length; i++) {
+                    chr = buffer[i];
+                    if (chr == 10 && tstart != i - 1) {
+                        if (buffer[i - 1] == 13) {
+                            nb = buffer.slice(tstart, i - 1);
+                        } else {
+                            nb = buffer.slice(tstart, i);
                         }
-                        
-                        console.log("nb:"+nb);
-                        if(nb.length>0){
+
+                       // console.log("nb:" + nb);
+                        if (nb.length > 0) {
                             list.push(nb)
                         }
-                        tstart=i+1;
+                        tstart = i + 1;
                     }
                 }
-                for(var i=list.length-1;i>=0;i--){
-                    var skey=list[i].toString();
-                    console.log(".,,,,,,length:"+skey.length+","+mkey.length);
-                    console.log("v:"+skey)
-                    if(skey.substring(0,mkey.length)==mkey){
-                        console.log("asldjflaksdjl;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;2");
-                        var proto="HTTP/1.1 101 Web Socket Protocol Handshake";
-                        var Upgrade="Upgrade: WebSocket";
-                        var remoteAddr="Sec-WebSocket-Origin: http://127.0.0.1";
-                        var wsremoteAddr="Sec-WebSocket-Location: ws://localhost:8100/";
-                        stream.write(proto);
-                        stream.write("\r\n");
-                       /* stream.write(Upgrade);
-                        stream.write("\r\n");
-                        stream.write(remoteAddr);
-                        stream.write("\r\n");
-                        stream.write(wsremoteAddr);
-                        stream.write("\r\n");
-                        stream.write("1234567890qazwsx");*/
-                        
-                      //  stream.end();
+                for (var i = list.length - 1; i >= 0; i--) {
+                    var skey = list[i].toString();
+
+                    if (skey.substring(0, mkey.length) == mkey) {
+
+
+
+                        stream.write("HTTP/1.1 101 Switching Protocols\r\n");
+                        stream.write("Upgrade: websocket\r\n");
+                        stream.write("Connection: Upgrade\r\n");
+                        stream.write("Date: " + new Date() + "\r\n");
+
+                        var nm = skey.replace(mkey, "").trim();
+                        console.log('mm;' + nm + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11");
+                        var hash = crypto.createHash("sha1");
+                        hash.update(nm + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11");
+                        var ws_accept = hash.digest('base64');
+
+                        console.log(ws_accept);
+                        stream.write("Sec-WebSocket-Accept: " + ws_accept + "\r\n");
+                        stream.write("WebSocket-Origin: http://localhost\r\n");
+                        stream.write("http://localhost:8100/testWebSocket.html\r\n");
+
                         stream.resume();
+                        stream.end();
+                        console.log("  stream.end();")
                         return;
                     }
                 }
-                stream.write("abceefig");
+                console.log("=====================================================");
+                stream.write("<a>");
+
+                stream.resume();
                  stream.end();
-                 stream.resume();
             });
             stream.on('end', function() {
-                stream.write('goodbye\r\n');
+                stream.write('==========================goodbye\r\n');
                 stream.end();
             });
             stream.on("drain", function() {
                 console.log("drain>>>");
             })
-             stream.on("error", function(e) {
-                console.log("error>>>"+e);
+            stream.on("error", function(e) {
+                console.log("error>>>" + e);
             })
         } catch (e) {
             console.log(e.message);
@@ -103,7 +114,7 @@
 
     });
     server.listen(8124, 'localhost');
-    server.on("stream",function(stream){
+    server.on("stream", function(stream) {
         console.log("--------------------");
     })
     //server.listen(8110);
