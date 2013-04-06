7d56669f696ec676ac058c5eeeaf13ea579a7fea
leo <cwq0312@163.com>
2013-04-06 16:42:35 +0800

    调整map类

--- src/Qmiks.Util.Map.js
+++ src/Qmiks.Util.Map.js
@@ -3,10 +3,9 @@
  * @email:cwq0312@163.com
  * @version:0.91.008
  */
-//server
-
+// server
 (function(Q) {
-	//加载其它模块
+	// 加载其它模块
 	require("./Qmiks.Log");
 	require("./Qmiks.Util");
 
@@ -16,34 +15,33 @@
 		me.length = 0;
 	}
 	Q.extend(Map.prototype, {
-		get: function(key) {
-
-			return this._map[key];
-		},
-		put: function(key, value) {
-			if (this._map[key] == null) {
-				this.length++;
-			}
-			this._map[key] = value;
-			return this;
-		},
-		rm: function(key) {
-			this.length--;
-			delete this._map[key];
-		},
-		size: function() {
-			return this.length;
-		},
-		containsKey: function(key) {
-			return this._map[key] != null;
-		},
-		//迭代
-		iterator: function(callback) {
-			for (var key in this._map) {
-				callback(key, this._map[key]);
-			}
-		}
-	});
+				get : function(key) {
+					return this._map[key];
+				},
+				put : function(key, value) {
+					if (this._map[key] == null) {
+						this.length++;
+					}
+					this._map[key] = value;
+					return this;
+				},
+				rm : function(key) {
+					this.length--;
+					delete this._map[key];
+				},
+				size : function() {
+					return this.length;
+				},
+				containsKey : function(key) {
+					return this._map[key] != null;
+				},
+				// 迭代
+				iterator : function(callback) {
+					for (var key in this._map) {
+						callback(key, this._map[key]);
+					}
+				}
+			});
 	Q.Util.Map = Map;
 	module.exports = Map;
 
