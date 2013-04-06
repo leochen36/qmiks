b983590dd9e89705e902b7eb2f7fca5f678b29c7
leo <cwq0312@163.com>
2013-04-06 12:21:55 +0800

    增加uuid计算

--- src/Qmiks.Util.js
+++ src/Qmiks.Util.js
@@ -6,16 +6,17 @@
 //server
 
 (function(Q) {
-    //加载其它模块
-    require("./Qmiks.Log");
-    Q.Util = {};
-    var Queue = require("./Qmiks.Util.Queue");
-    var Map = require("./Qmiks.Util.Map");
-    var uuid = require("./Qmiks.Util.uuid");
+	//加载其它模块
+	require("./Qmiks.Log");
+	Q.Util = {};
+	var Queue = require("./Qmiks.Util.Queue");
+	var Map = require("./Qmiks.Util.Map");
+	var uuid = require("./Qmiks.Util.uuid");
 
-    Q.extend(Q.Util,{
-    })
+	Q.extend(Q.Util, {
+		uuid: uuid
+	})
 
-    module.exports = Q.Util;
+	module.exports = Q.Util;
 
 })(require("./Qmiks"));
