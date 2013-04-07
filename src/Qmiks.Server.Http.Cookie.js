5e9331bb5458d8c89b4409160aab4f7126147469
leo <cwq0312@163.com>
2013-04-07 11:22:23 +0800

    cookie过期时间,从配置里取

--- src/Qmiks.Server.Http.Cookie.js
+++ src/Qmiks.Server.Http.Cookie.js
@@ -14,16 +14,16 @@
 	var Http = require("./Qmiks.Server.Http");
 	var Log = require("./Qmiks.Log");
 	var Config = require("./Qmiks.Server.Http.Config");
-	var log = new Log("Qmiks.Server.Http");
 
 	// 运行参数
-	var timeout = 7 * 24 * 60 * 60;// 过期时间
+	var log = new Log("Qmiks.Server.Http");
+	var timeout = Config.cookie.timeout;// cookie保存时间
 	function Cookie(name, value, expires, domain, path, secure) {
 		var me = this, cookie = [];
 		// me._data = querystring.parse(value);
 		me.name = name;
 		me.value = value;
-		expires = expires || timeout;// 单位秒
+		expires = expires || timeout;// 单位分
 		// cookie有效期时间
 		if (expires) {
 			expires = parseInt(expires);
@@ -75,7 +75,7 @@
 					if (expires) {
 						expires = parseInt(expires);
 						var today = new Date();
-						var time = today.getTime() + expires * 1000;
+						var time = today.getTime() + expires * 60000;
 						var new_date = new Date(time);
 						var expiresDate = new_date.toGMTString(); // 转换成GMT
 						// 格式。
